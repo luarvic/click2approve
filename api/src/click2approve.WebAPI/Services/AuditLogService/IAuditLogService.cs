1a3aaa483a60e6432a7775fd973c33b05a707e51
Rosti Semenov <rs4talk@gmail.com>
2026-02-13 14:32:29 -0500

    git: fix folder casing

--- api/src/click2approve.WebAPI/Services/AuditLogService/IAuditLogService.cs
+++
@@ -1,9 +0,0 @@
-namespace Click2Approve.WebAPI.Services.AuditLogService;
-
-/// <summary>
-/// Defines a contract for a service that manages the audit log.
-/// </summary>
-public interface IAuditLogService
-{
-    Task LogAsync(string who, DateTime when, string what, string jsonData, CancellationToken cancellationToken);
-}
