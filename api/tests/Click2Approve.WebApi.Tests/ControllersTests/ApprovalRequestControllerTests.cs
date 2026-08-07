2ac6f99283cfae61ceb445c586eaff9415a0b501
Rosti Semenov <rs4talk@gmail.com>
2026-08-07 19:01:47 -0400

    feat: update approval request tests to use private person display name for requested by field

--- api/tests/Click2Approve.WebApi.Tests/ControllersTests/ApprovalRequestControllerTests.cs
+++ api/tests/Click2Approve.WebApi.Tests/ControllersTests/ApprovalRequestControllerTests.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using Click2Approve.Application.Helpers;
 using Click2Approve.Application.Models.DTOs;
 using Click2Approve.Domain.Models;
 using Click2Approve.WebApi.Tests.Extensions;
@@ -157,7 +158,7 @@ public class ApprovalRequestControllerTests(CustomWebApplicationFactory<Program>
 
         var task = await taskResponse.Content.ReadFromJsonAsync<ApprovalRequestTaskDetailDto>();
         Assert.NotNull(task);
-        Assert.Equal(requester.Email, task.RequestedByDisplayName);
+        Assert.Equal(DisplayNameHelpers.PrivatePersonDisplayName, task.RequestedByDisplayName);
         Assert.Equal(approvalRequest.RevisionNumber, task.RevisionNumber);
         Assert.NotNull(task.ApprovalRequest);
         Assert.Collection(task.ApprovalRequest.Steps.OrderBy(step => step.Sequence),
@@ -258,7 +259,7 @@ public class ApprovalRequestControllerTests(CustomWebApplicationFactory<Program>
         Assert.True(taskResponse.IsSuccessStatusCode, await taskResponse.Content.ReadAsStringAsync());
         var taskDetail = await taskResponse.Content.ReadFromJsonAsync<ApprovalRequestTaskDetailDto>();
         Assert.NotNull(taskDetail);
-        Assert.Equal(requester.Email, taskDetail.RequestedByDisplayName);
+        Assert.Equal(DisplayNameHelpers.PrivatePersonDisplayName, taskDetail.RequestedByDisplayName);
         Assert.NotNull(taskDetail.ApprovalRequest);
         Assert.Single(taskDetail.ApprovalRequest.Steps);
         Assert.Single(Assert.Single(taskDetail.ApprovalRequest.Steps).Tasks);
